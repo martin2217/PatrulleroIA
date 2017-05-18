5404c271c90790263a87e45f864f465db2780c45
Hugo Hardy <huguihardy@gmail.com>
2017-05-18 19:32:10 -0300

    Dominio Nodo

--- src/frsf/cidisi/exercise/patrullero/dominio/Nodo.java
+++ src/frsf/cidisi/exercise/patrullero/dominio/Nodo.java
@@ -1,5 +1,16 @@
 package frsf.cidisi.exercise.patrullero.dominio;
 
 public class Nodo {
+ private String nombre;
+ 
+ public Nodo(){
+ }
 
+ public String getNombre(){
+	 return nombre;
+ }
+ 
+ public void setNombre(String unNombre){
+	 nombre = unNombre;
+ }
 }
